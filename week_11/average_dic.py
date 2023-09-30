df2534a4fa123ed9a2e550abd2e537eff6724962
samtah <samaneh.taheri@billogram.com>
2023-09-30 16:32:24 +0200

    week 11 o

---
+++ week_11/average_dic.py
@@ -0,0 +1,6 @@
+data = [{"math": 13.5, "farsi": 14, "science":18, "physicall_education":17, "Art":10},
+        {"math":13, "farsi":19, "science":18, "physicall_education":16,"Art": 19},
+        {"math":19, "farsi":18.5, "science":16, "physicall_education":15.5, "Art":18},
+        {"math":12, "farsi":19,"science": 14.5, "physicall_education":16.5,"Art": 18},
+        {"math":17, "farsi":17,"science": 18.5,"physicall_education": 14, "Art":19.5}]
+print(data)
