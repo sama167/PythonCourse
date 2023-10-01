4f11e7da4af3b64f13cd41db23e484b8def94339
samtah <samaneh.taheri@billogram.com>
2023-10-01 11:00:08 +0200

    week 11, faraz and hadi's homework

--- week_11/average_dic.py
+++ week_11/average_dic.py
@@ -1,6 +1,18 @@
-data = [{"math": 13.5, "farsi": 14, "science":18, "physicall_education":17, "Art":10},
+def sort_best_grade(student)->float:
+    return sorted(student.values(), reverse=True)
+
+data = [{"math": 13.5, "science":18, "physicall_education":17, "Art":10, "farsi": 14 },
         {"math":13, "farsi":19, "science":18, "physicall_education":16,"Art": 19},
         {"math":19, "farsi":18.5, "science":16, "physicall_education":15.5, "Art":18},
         {"math":12, "farsi":19,"science": 14.5, "physicall_education":16.5,"Art": 18},
-        {"math":17, "farsi":17,"science": 18.5,"physicall_education": 14, "Art":19.5}]
-print(data)
+        {"science": 18.5, "math":17, "farsi":17,"physicall_education": 14, "Art":19.5}]
+
+
+for grade in data:
+    sum = grade["math"] + grade["farsi"] + grade["science"] + grade["physicall_education"] + grade["Art"]
+    avg = sum / 5
+    print(avg)
+
+for grade in data:
+    print(grade.values())
+    print(sort_best_grade(grade)[0])
