46f0652895241ca0626ea20b0cc3c269a9ec3e9a
samtah <samaneh.taheri@billogram.com>
2023-10-01 11:06:22 +0200

    week 12, homework description

---
+++ week_12/sort_avg.py
@@ -0,0 +1,8 @@
+# write a program to calculate each student's average of three subjects (math, farsi and science),
+#  then sort the average and print the worst and best average in the output.
+
+data = [{"math": 13.5, "science":18, "physicall_education":17, "Art":10, "farsi": 14 },
+        {"math":13, "farsi":19, "science":18, "physicall_education":16,"Art": 19},
+        {"math":19, "farsi":18.5, "science":16, "physicall_education":15.5, "Art":18},
+        {"math":12, "farsi":19,"science": 14.5, "physicall_education":16.5,"Art": 18},
+        {"science": 18.5, "math":17, "farsi":17,"physicall_education": 14, "Art":19.5}]
