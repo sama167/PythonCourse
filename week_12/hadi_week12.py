d6e922aa2ea1c548a5d4013d7998454283f7912f
samtah <samaneh.taheri@billogram.com>
2023-10-14 09:14:16 +0200

    week 12

---
+++ week_12/hadi_week12.py
@@ -0,0 +1,25 @@
+data = [{"name":"ali","math": 13.5, "science":18, "physicall_education":17, "Art":10, "farsi": 14 },
+        {"name":"mamad","math":13, "farsi":19, "science":18, "physicall_education":16,"Art": 19},
+        {"name":"hassan","math":19, "farsi":18.5, "science":16, "physicall_education":15.5, "Art":18},
+        {"name":"farank","math":12, "farsi":19,"science": 14.5, "physicall_education":16.5,"Art": 18},
+        {"name":"sarah","science": 18.5, "math":17, "farsi":17,"physicall_education": 14, "Art":19.5}]
+all_avg=[]
+for grade in data:
+    sum = grade["math"] + grade["farsi"] + grade["science"] 
+    avg = sum / 3
+    print(avg,grade["name"])
+    all_avg.append(avg)
+all_avg.sort()
+# print(all_avg)
+# print("the best avg",all_avg[-1])
+# print("the wort avg",all_avg[0])
+data.append(all_avg)
+print(data)
+
+def functiooon(z):
+    return z["all_avg"]
+
+data.sort(key=functiooon)
+# print(data)
+
+
--- week_12/sort_avg.py
+++ week_12/sort_avg.py
@@ -1,20 +1,27 @@
 # write a program to calculate each student's average of three subjects (math, farsi and science),
 #  then sort the average and print the worst and best average in the output.
 
-data = [{"math": 13.5, "science":18, "physicall_education":17, "Art":10, "farsi": 14 },
-        {"math":13, "farsi":19, "science":18, "physicall_education":16,"Art": 19},
-        {"math":19, "farsi":18.5, "science":16, "physicall_education":15.5, "Art":18},
-        {"math":12, "farsi":19,"science": 14.5, "physicall_education":16.5,"Art": 18},
-        {"science": 18.5, "math":17, "farsi":17,"physicall_education": 14, "Art":19.5}]
+def my_sort(member):
+    return member["average"]
+
+data = [{"name":"ali","math": 13.5, "science":18, "physicall_education":17, "Art":10, "farsi": 14 },
+        {"name":"mamad","math":13, "farsi":19, "science":18, "physicall_education":16,"Art": 19},
+        {"name":"hassan","math":19, "farsi":18.5, "science":16, "physicall_education":15.5, "Art":18},
+        {"name":"farank","math":12, "farsi":19,"science": 14.5, "physicall_education":16.5,"Art": 18},
+        {"name":"sarah","science": 18.5, "math":17, "farsi":17,"physicall_education": 14, "Art":19.5}]
 
 all_avg = []
 
 for student in data:
     sum = student["math"] + student["farsi"] + student["science"]
     avg = sum/3
-    all_avg.append(avg)
+    name = student["name"]
+    print(student["name"], " has avg ", avg)
+    all_avg.append({"average": avg, "username": name})
 
-all_avg.sort()
-print(all_avg.sort())
+print(all_avg)
+
+all_avg.sort(key=my_sort)
+print(all_avg)
 print("best avg ", all_avg[-1])
 print("worst avg ", all_avg[0])
