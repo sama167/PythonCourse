4338625aa4e8b3658e4aef7ef5c4c340e571d0ec
sama167 <samanetaheri@gmail.com>
2024-03-03 20:43:56 +0100

    Add incomplete version of rock_scissors_paper game

---
+++ term_2/rock_scissors_paper.py
@@ -0,0 +1,22 @@
+import random
+
+human_choice = input("Enter your choice (rock, paper, scissors): ")
+
+random_choice = random.choice(["rock", "paper", "scissors"])
+
+
+
+print(f"You chose {human_choice}, computer chose {computer_choice}.")
+
+if human_choice == computer_choice:
+  print("It's a tie!")
+  print("-------------")
+  print("-------------")
+  print("-------------")
+  print("-------------")
+elif human_choice == "rock" and computer_choice == "paper":
+  print("Computer wins!")
+elif human_choice == "scissors" and computer_choice == "paper":
+  print("You win!")
+else:
+  print("Invalid input. Please enter either rock, paper, or scissors.")
